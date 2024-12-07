606374cc752769c860ca38d641b7a1f2522b8ebb
Toufeeq khan <164334991+Toufeeqkhan@users.noreply.github.com>
2024-12-07 20:36:43 +0500

    Added more to the basic subject card

--- src/assets/components/SubjectCard.jsx
+++ src/assets/components/SubjectCard.jsx
@@ -1,13 +1,29 @@
 import './SubjectCard.css'
 
-export default function SubCard(props){
+export default function SubCard({lectureNumber,time,subjectName}){
     return (
         <div className="sub-card">
+
             <div className="top">
-            <p>Lecture {props.lectureNumber}</p>
-            <p>{props.time}</p>
+            <p>Lecture {lectureNumber}</p>
+            <p>{time}</p>
             </div>
-            <h3>{props.subjectName}</h3>
+            <h2 className='subject-name'>{subjectName}</h2>
+
+            <form className='book-form' action="#">
+            <div className="topic-input">
+            <label htmlFor="topic">Topic</label>
+            <input id="topic" type="text" />
+            </div>
+
+            <div className="assignment-input">
+            <label htmlFor="assignment">Assignment</label>
+            <input id="assignment" type="text" />
+            </div>
+
+            <button className='add-btn'>Add</button>
+            </form>
         </div>
     )
 }
+
